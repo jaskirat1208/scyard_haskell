56dd3f72cbd275cd8e81f7bfe1dc5a88fedfe54d
jaskirat1208 <jaskiratsingh1208@gmail.com>
2018-04-03 18:57:47 +0530

    MOVES working

--- Players.hs
+++ Players.hs
@@ -1,16 +1,16 @@
 module Players where 
 import Data.Graph.Inductive
-
-
+import GeeksLand 
+import Data.List
 type Name  = String
-type Taxi_t= Int
-type Rail_t= Int
-type Bus_t = Int
+type Foot= Int
+type Ropeway= Int
+type Heli = Int
 type Black_t = Int
 type Position = Node
-type Detective = (Name,Position,Taxi_t,Rail_t, Bus_t)
-type Criminal = (Name,Position,Taxi_t,Rail_t,Bus_t,Black_t)
-
+type Detective = (Name,Position,Foot,Ropeway, Heli)
+type Criminal = (Name,Position,Foot,Ropeway,Heli,Black_t)
+type GameState = ([Detective],Criminal)
 mark::Detective 
 mark = ("Mark", 2,15, 7, 10)
 
@@ -28,3 +28,97 @@ crick = ("Crick", 5, 15, 7, 10)
 
 tom::Criminal
 tom = ("Tom", 6, 7, 5, 4, 2)
+
+players::GameState
+players = ([mark,marcus,mike,james,crick],tom)
+
+
+--Deletes an element from its index
+deleteAt idx xs = lft ++ rgt  where (lft, (_:rgt)) = splitAt idx xs
+
+
+getDetective gs turn = ((fst gs) !! (turn-1))
+getKiller gs turn    = snd gs
+
+getKillerPosition gs turn = x where (a,x,c,d,e,f) = getKiller gs turn
+getDetectivePosition gs turn = x where (a,x,c,d,e) = getDetective gs turn
+
+
+checkDetectiveTicket gs turn "foot" = c/=0 where  (a,b,c,d,e) = getDetective gs turn 
+checkDetectiveTicket gs turn "ropeway" = d/=0 where (a,b,c,d,e) = getDetective gs turn
+checkDetectiveTicket gs turn "heli" = e/=0 where (a,b,c,d,e) = getDetective gs turn
+checkDetectiveTicket gs turn ticket = False
+
+checkKillerTicket gs turn "foot" = c/=0 where  (a,b,c,d,e,f) = getKiller gs turn
+checkKillerTicket gs turn "ropeway" = d/=0 where (a,b,c,d,e,f) = getKiller gs turn
+checkKillerTicket gs turn "heli" = e/=0 where (a,b,c,d,e,f) = getKiller gs turn
+checkKillerTicket gs turn "black" = f/=0 where (a,b,c,d,e,f) = getKiller gs turn 
+checkKillerTicket gs turn ticket = False
+
+
+node_check gs node turn | turn == 6 = find (==node) (neighbors geeksland (getKillerPosition gs turn)) /= Nothing
+                        | otherwise = find (==node) (neighbors geeksland (getDetectivePosition gs turn)) /= Nothing
+
+
+ticket_exis_check::GameState ->String->Int->Bool
+ticket_exis_check gs ticket turn | turn == 6 = checkKillerTicket gs turn ticket 
+                                 | otherwise = checkDetectiveTicket gs turn ticket
+
+genLabelFromTicket "foot" = (1,0,0) 
+genLabelFromTicket "ropeway" = (0,1,0) 
+genLabelFromTicket "heli" = (0,0,1) 
+
+--There is an edge corresponding to the ticket given
+
+match_check gs node "black" 6 = True
+match_check gs node ticket 6 = (find (==(node,getKillerPosition gs 6,genLabelFromTicket ticket)) (labEdges geeksland) /=Nothing) || (find (==(getKillerPosition gs 6,node,genLabelFromTicket ticket)) (labEdges geeksland) /=Nothing)
+match_check gs node ticket turn = (find (==(node,getDetectivePosition gs turn,genLabelFromTicket ticket)) (labEdges geeksland) /=Nothing) || (find (==(getDetectivePosition gs turn,node,genLabelFromTicket ticket)) (labEdges geeksland) /=Nothing)
+
+updateState gs x "black" 6 = ((fst gs),(a,x,c,d,e,f-1)) where (a,b,c,d,e,f) = getKiller gs 6   
+updateState gs x "foot" 6  = ((fst gs),(a,x,c-1,d,e,f)) where (a,b,c,d,e,f) = getKiller gs 6   
+updateState gs x "ropeway" 6  = ((fst gs),(a,x,c,d-1,e,f)) where (a,b,c,d,e,f) = getKiller gs 6   
+updateState gs x "heli" 6  = ((fst gs),(a,x,c,d,e-1,f)) where (a,b,c,d,e,f) = getKiller gs 6   
+updateState gs x "heli"     turn = ((take (turn-1) (fst gs)) ++ [(a,x,c,d,e-1)] ++ (drop turn (fst gs)),(l,m,n,o,p+1,q)) where  (a,b,c,d,e) = getDetective gs turn
+                                                                                                                                (l,m,n,o,p,q) = getKiller gs turn
+updateState gs x "foot"     turn = ((take (turn-1) (fst gs)) ++ [(a,x,c-1,d,e)] ++ (drop turn (fst gs)),(l,m,n+1,o,p,q)) where  (a,b,c,d,e) = getDetective gs turn
+                                                                                                                                (l,m,n,o,p,q) = getKiller gs turn
+updateState gs x "ropeway"  turn = ((take (turn-1) (fst gs)) ++ [(a,x,c,d-1,e)] ++ (drop turn (fst gs)),(l,m,n,o+1,p,q)) where  (a,b,c,d,e) = getDetective gs turn
+                                                                                                                                (l,m,n,o,p,q) = getKiller gs turn
+
+
+check_valid_move::GameState -> Node -> String -> Int -> (Bool,GameState)
+check_valid_move gs x y turn | (node_check gs x turn ) && (ticket_exis_check gs y turn) && (match_check gs x y turn ) = (True,(updateState gs x y turn))
+                             | otherwise = (False,gs)
+
+-- CHECK_VALID MOVE SHALL perform sequential checks: node check, ticket and edge match check and ticket existence check,
+move::GameState->Int->IO(GameState)
+move players 6 = do 
+                    x_str <- getLine
+                    let x = (read x_str ::Int)
+                    let orig_killer = snd players
+                    let new_killer = (a,x,c,d,e,f) where (a,b,c,d,e,f) = orig_killer
+                    print new_killer
+                    return (fst players,new_killer)
+
+move players y  | y>6 || y<=0 = do
+                                    print "Please enter a number between 1 to 6"
+                                    return players 
+
+                | otherwise    = do
+                                    let changed_detective = (fst players) !! (y-1)
+                                    putStrLn "Enter your destination."
+                                    x_str <- getLine
+                                    putStrLn "Please Enter your ticket(RED,BLUE,GREEN,BLACK(only for Tom))."
+                                    y_str <- getLine
+                                    let x = (read x_str ::Int)
+                                    if(not (fst (check_valid_move players x y_str y) ))
+                                        then do
+                                                print "ILLEGAL MOVE. Please move again."
+                                                move players y
+                                                return players
+                                    else
+                                        do
+                                            let new_changed_detective = (a,x,c,d,e) where (a,b,c,d,e) = changed_detective
+                                            let newList = (take (y-1) (fst players) ) ++[new_changed_detective] ++(drop y (fst players))
+                                            print (newList, snd players)
+                                            return (newList,snd players)
--- ScyardSetup.hs
+++ ScyardSetup.hs
@@ -5,34 +5,33 @@ module ScyardSetup where
 import Data.Graph.Inductive
 import Data.Graph.Inductive.Example
 import Players
+import Data.List
 state::Gr Int (Int, Int, Int, Int)
 state = mkGraph (zip [1..103] [1..103]) [(1,2,(1,0,1,0)), (1,3,(1,0,1,1))]
 --main_hill: Nodes from 79 to 103
 --hillocks: Nodes from 1 to 78
 
 
+--get_positions_list::GameState -> [Node]
 
-setup_game = do
-                print mark
-                print marcus
-                print mike
-                print james
-                print crick
+detective_pose::Detective->Node
+detective_pose det = x where (a,x,c,d,e)=det
+get_positions_list gs = (killer_pose (snd gs)) ++ (map detective_pose (fst gs)) 
 
-tom_is_caught = True
-make_next_move y | tom_is_caught == True = do
-                                                putStrLn "Game Ends"
-                 | y `mod` 6 == 0 = do 
-                                                putStrLn "Mark's Turn"     
-                 | y `mod` 6 == 1 = do          
-                                                putStrLn "Marcus's Turn"
-                 | y `mod` 6 == 2 = do 
-                                                putStrLn "Mike's Turn"
-                 | y `mod` 6 == 3 = do
-                                                putStrLn "James' Turn"
-                 | y `mod` 6 == 4 = do 
-                                                putStrLn "Crick's Turn"
-                 | y `mod` 6 == 5 = do
-                                                putStrLn "Tom's Turn"
-----------------------------------------------------------------------------------------------------------------------------------------------------
-----------------------------------------------------------------------------------------------------------------------------------------------------
+killer_pose::Criminal->[Node]
+killer_pose kil = [l] where (a,l,c,d,e,f) = kil
+
+tom_is_caught::GameState ->Bool
+tom_is_caught gs | find (==(head (get_positions_list gs))) (tail (get_positions_list gs)) /= Nothing = True
+                 | otherwise = False
+
+game_init::GameState -> Int -> IO(Int)
+game_init players x = if( tom_is_caught players == True)   then   
+                            do 
+                                print "MARK IS CAUGHT. GAME ENDS"
+                                return 1
+                        else
+                            do 
+                                new_gs <- move players (1+x)
+                                flag <- game_init new_gs ((x+1) `mod` 6)
+                                return flag
