e24f7a7ceaa3b8848480f709f1bc37124d30683f
jaskirat1208 <jaskiratsingh1208@gmail.com>
2018-04-05 14:42:03 +0530

    Bug fixed. MAP created

--- GeeksLand.hs
+++ GeeksLand.hs
@@ -7,7 +7,7 @@ base_hillock i = mkGraph (vertices_of_base_hillock i) (edges_of_base_hillock i)
 vertices_of_base_hillock i = (zip [i..(i+12)] [i..])
 --There is a slight change: Black tickets have been removed: Now black tickets are only used to navigate hiding ticket from the detectives. No special black ticket route is there
 bus_edges i = [(i,i+2,(0,1,0)),(i,i+3,(0,1,0)),(i,i+1,(0,1,0)),(i,i+4,(0,1,0)),(i+7,i+2,(0,1,0)),(i+9,i+3,(0,1,0)),(i+5,i+1,(0,1,0)),(i+11,i+4,(0,1,0)),(i+8,i+2,(0,1,0)),(i+10,i+3,(0,1,0)),(i+6,i+1,(0,1,0)),(i+12,i+4,(0,1,0))] 
-taxi_edges i = [(i,i+1,(1,0,0)) | i <- [2..4]++[6..12] ] ++ [(5,2,(1,0,0)),(13,6,(1,0,0))]
+taxi_edges i = [(i+j-1,i+j,(1,0,0)) | j <- [2..4]++[6..12] ] ++ [(i+4,i+1,(1,0,0)),(i+12,i+5,(1,0,0))]
 edges_of_base_hillock i = (bus_edges i) ++ (taxi_edges i)
 
 
